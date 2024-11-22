226a439ed4a52fa270c5ab6d5a8f072dc062c3cb
jean9710 <jean9710@proten.co.kr>
2024-11-22 13:00:46 +0900

    AdminProductPage 수정

--- src/page/AdminProductPage/AdminProductPage.jsx
+++ src/page/AdminProductPage/AdminProductPage.jsx
@@ -98,11 +98,11 @@ const AdminProductPage = () => {
               Add New Item +
             </button>
             <div className='item-count-dropdown'>
-              <select id='items-per-page' onChange={handleLimitChange}>
+              <select id='items-per-page' value={searchQuery.limit} onChange={handleLimitChange}>
+                <option value='5'>5</option>
                 <option value='10'>10</option>
                 <option value='20'>20</option>
                 <option value='30'>30</option>
-                <option value='50'>50</option>
               </select>
             </div>
           </div>
